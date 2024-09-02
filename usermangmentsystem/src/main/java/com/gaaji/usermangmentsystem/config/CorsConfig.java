f2d7d290c3066d5d54964aee0dddae07268dea35
gaji12345 <gajimax12345@gmail.com>
2024-09-02 07:50:13 +0530

    CorosConfig class done

---
+++ usermangmentsystem/src/main/java/com/gaaji/usermangmentsystem/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.gaaji.usermangmentsystem.config;/*  gaajiCode
+    99
+    02/09/2024
+    */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
