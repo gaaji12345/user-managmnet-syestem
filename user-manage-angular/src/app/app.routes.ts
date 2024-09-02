c20abd0033a9cf1233360422d55a0f7d147ceb51
gaji12345 <gajimax12345@gmail.com>
2024-09-02 13:44:45 +0530

    updated

--- user-manage-angular/src/app/app.routes.ts
+++ user-manage-angular/src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import {CanActivateFn, Router, Routes} from '@angular/router';
 import {inject} from "@angular/core";
 import {UsersService} from "./users.service";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {usersGuard} from "./users.guard";
+import {UpdateuserComponent} from "./updateuser/updateuser.component";
+import {UserlistComponent} from "./userlist/userlist.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [usersGuard]},
+  {path: 'update/:id', component: UpdateuserComponent},
+  {path: 'users', component: UserlistComponent},
+  {path: '**', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
 
 
 export const adminGuard: CanActivateFn = (route, state) => {
