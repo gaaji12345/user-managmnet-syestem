defeb942a068e3cf0ce0bb94e827fc729a2d5cdd
gaji12345 <gajimax12345@gmail.com>
2024-09-02 07:17:06 +0530

    test commit

---
+++ usermangmentsystem/src/main/java/com/gaaji/usermangmentsystem/UsermangmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.gaaji.usermangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermangmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermangmentsystemApplication.class, args);
+	}
+
+}
---
+++ usermangmentsystem/src/test/java/com/gaaji/usermangmentsystem/UsermangmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.usermangmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermangmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
