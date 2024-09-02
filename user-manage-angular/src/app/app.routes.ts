6faf1d02ca35f29bdcd67fbd7ecb1a38f1ab5a67
gaji12345 <gajimax12345@gmail.com>
2024-09-02 10:23:31 +0530

    rote-ts done

--- user-manage-angular/src/app/app.routes.ts
+++ user-manage-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {CanActivateFn, Router, Routes} from '@angular/router';
+import {inject} from "@angular/core";
+import {UsersService} from "./users.service";
 
 export const routes: Routes = [];
+
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  if (inject(UsersService).isAdmin()) {
+    return true;
+  }else{
+    inject(Router).navigate(['/login'])
+    return false
+  }
+};
